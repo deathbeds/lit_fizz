4bb4399fbaee7204da6054d6a5ee7f556436c874
tonyfast <tony.fast@gmail.com>
2020-10-05 07:18:24 -0400

    lazy

--- lit_fizz/__init__.py
+++ lit_fizz/__init__.py
@@ -1,6 +1,6 @@
 """a fizz buzz program."""
 __version__ = "0.1.0"
 
-with __import__("tingle").Markdown():
+with __import__("tingle").Markdown(lazy=True):
     from .readme import *
     from . import readme
